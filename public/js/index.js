c2d2b0c31f97156c1c9231fa12c24cf30463ba29
Yashwanth Raju <yashwanthraju07@gmail.com>
2021-08-12 20:51:28 +0530

    made some changes in js file

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ const weatherForm=document.querySelector("form")
 const search=document.querySelector("input")
 const messageOne=document.querySelector("#message-1")
 const messageTwo=document.querySelector("#message-2")
-console.log(messageTwo.textContent="master")
+
 
 weatherForm.addEventListener('click',(e)=>{
     e.preventDefault()
